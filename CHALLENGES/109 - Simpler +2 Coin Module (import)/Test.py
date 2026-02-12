87dd9c34cf6f0f51de465f45a68b6f80743b19aa
mcoradini1 <mcoradini1@hotmail.com>
2026-02-12 09:09:31 +0000

    Update (Organising)

--- CHALLENGES/109 - Simpler +2 Coin Module (import)/Test.py
+++ CHALLENGES/109 - Simpler +2 Coin Module (import)/Test.py
@@ -1,6 +1,6 @@
 import coins
 p = float(input('Type your price: £'))
-print(f'2x the price is {coins.twice(p,True)}')
+print(f'2x the price is {coins.twice(p, True)}')
 print(f'Half of the price is {coins.half(p, True)}')
 print(f'Increasing the price by 10% I would have {coins.increase(p, 10, True)}')
 print(f'Decreasing the price by 10% I would have {coins.decrease(p, 10, True)}')
--- CHALLENGES/110 - Simpler +3 Coin Module (import)/Test.py
+++ CHALLENGES/110 - Simpler +3 Coin Module (import)/Test.py
@@ -1,4 +1,4 @@
 import coins
 test_variable = float(input('Please set the product value: '))
-coins.resume(test_variable,10,10)
+coins.resume(test_variable, 10, 10)
 
